b6384a5fceb9922131f909bdd7b3618648c7ae10
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-09-13 15:04:16 +0700

    Membuat Fungsi Pemanggilan Nama Tabel Otomatis

--- app/application/core/MY_Controller.php
+++ app/application/core/MY_Controller.php
@@ -1,10 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Controllername extends CI_Controller
+class MY_Controller extends CI_Controller
 {
     public function __construct()
     {
+        parent::__construct();
+        
         $model = strtolower(get_class($this));
         if (file_exists(APPPATH.'models/'.$model.'_model.php')) {
             $this->load->model($model. '_model', $model);
